b77eccfa979cb7564abc146c4e0c5c548afe6de4
Racloody <gageotelouan@pm.me>
2024-09-11 11:10:52 +0200

    Commenter et metre au propre

--- 02. Récursivité/exercice_3.1.py
+++ 02. Récursivité/exercice_3.1.py
@@ -1,5 +1,12 @@
+"""Programme qui calcule une puissance choisie par l'utilisateur sur un nombre également choisi par ce dernier."""
+n = int(input("Definir un nombre n : ")) #L'utilisateur choisit un nombre n
+a = int(input("Définir sa puissance a : ")) #L'utilisateur choisit la puissance a
+
+#La fonction calcule la puissance a du nombre n
 def puissance (a,n):
-    if a == 0:
-        return 1
-    else :
-        return a*puissance(a, n-1)
+    if isinstance (n, int): #On verifie que n est entier
+        if n == 0:
+            return 1 # Si n = 0 On retourne 1
+        else :
+            return a*puissance(a, n-1) #Sinon on calcule la puissance
+print(puissance(a,n))
