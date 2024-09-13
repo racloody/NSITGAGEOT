1e424ea092dc27944d415a36c1fdd05f23f89f2d
Racloody <gageotelouan@pm.me>
2024-09-13 22:58:22 +0200

    correction

--- M_Blanqui/POO/poo tps.py
+++ M_Blanqui/POO/poo tps.py
@@ -42,14 +42,14 @@ class temps():
                 return "{}:{}:{}".format(hr_fin,min_fin,sec_fin)
         else:
             return "improssible à déterminer, entrez une heure inferieure à la première"
-heure_1 = input("Entrez une heure 1 sous le forme HH:MM:SS : ")
+heure_1 = input("Entrez une heure 1 sous la forme HH:MM:SS : ")
 hours1 , minutes1 , seconds1 = heure_1.split(':')
-heure_2 = input("Entrez une heure 2 sous le forme HH:MM:SS : ")
+heure_2 = input("Entrez une heure 2 sous la forme HH:MM:SS : ")
 hours2 , minutes2 , seconds2 = heure_2.split(':')
 hours1 , minutes1 , seconds1, hours2 , minutes2 , seconds2 = int(hours1) , int(minutes1) , int(seconds1) , int(hours2) , int(minutes2) , int(seconds2)
 temps1=temps(hours1,minutes1,seconds1)
 temps2=temps(hours2,minutes2,seconds2)
-opé=input("Choisissez unz opération (+/-) : ")
+opé=input("Choisissez une opération (+/-) : ")
 if opé == "-":
     print(f"Le résultat est {temps1-temps2}")
 else:
