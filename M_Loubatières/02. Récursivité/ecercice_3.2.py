0ef99c5fcf5f8bcf3dd666b81c22957d9077fef7
Racloody <gageotelouan@pm.me>
2024-09-26 14:55:41 +0200

    j

--- M_Loubatières/02. Récursivité/ecercice_3.2.py
+++ M_Loubatières/02. Récursivité/ecercice_3.2.py
@@ -4,7 +4,7 @@ while n != "q":
 
     n = input("Definir un nombre n (q pour quitter) : ") #L'utilisateur choisit un nombre n
     fact = 1 #On initialise la variable fact avec 1
-    os.system('cls')
+
     #La fonction calcule la factorielle du nombre n
     def factorielle(n):
 
