54b2c7fa9032fdb236fc2943164b92f3b2aa7917
Racloody <gageotelouan@pm.me>
2024-09-26 14:55:23 +0200

    mm

---
+++ M_Blanqui/Structures de données/tad_liste_1.py
@@ -0,0 +1,38 @@
+class MaListe():
+    def __init__(self):
+        """
+        initialise un objet correspondant à une liste Python vide
+        """
+        self.liste=[]
+    
+    def isVide(self):
+        """
+        @Return : Booléen qui indique si la liste est vide
+        """
+        pass
+        
+    def ajoute(self,x):
+        """
+        Ajpoute l'élément x en tête de liste'
+        """
+        pass
+    
+    def __len__(self):
+        """
+        #return : Longueur de la liste
+        
+        """
+        pass
+    
+    def __getitem__(self,i):
+        """
+        @return : élément à la position i de la liste
+        """
+        pass
+    
+    def __str__(self):
+        """
+        @return : affiche les éléments de la liste dans l'ordre'
+        """
+        pass
+            
